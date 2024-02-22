121b83f4615dbf12668dcc746a59f17a6d49c7dd
AngelDeLeon200120 <angel.eliandeleon@gmail.com>
2024-02-21 21:58:06 -0600

    se modifico mongo y env en index

--- index.js
+++ index.js
@@ -1,61 +1,73 @@
-const express = require('express');
+const express = require("express");
 // import express from 'express';
 const app = express();
-const db =require('./db/dbconnect.js');
+const db = require("./db/dbconnect.js");
 // import {DB} from './db/dbconnect.js';
 // import cors from 'cors';
-const cors =require('cors');
-const mongoose =require('mongoose');
-const modeluser =require('./db/dbconnect.js');
-const urlFront =require('./config.js');
-const env =require('node:process');
-const { resolve } = require('node:path');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const modeluser = require("./db/dbconnect.js");
+const urlFront = require("./config.js");
+const env = require("node:process");
 
 // const PORT =process.env.PORT
 // const MONGGOD_CONNECT_URI=process.env.MONGGOD_CONNECT_URI
 
-env.PORT=4000;
-env.MONGGOD_CONNECT_URI='mongodb+srv://angeleliandeleon:angel2001@book-store-mern.xwkldkr.mongodb.net/MongoBD?retryWrites=true&w=majority&appName=Book-Store-MERN ';
+env.PORT = 4000;
+env.MONGGOD_CONNECT_URI =
+  "mongodb+srv://angeleliandeleon:angel2001@book-store-mern.xwkldkr.mongodb.net/MongoBD?retryWrites=true&w=majority&appName=Book-Store-MERN ";
 const PORT = process.env.PORT || env.PORT;
-const MONGGOD_CONNECT_URI=process.env.MONGGOD_CONNECT_URI || env.MONGGOD_CONNECT_URI;
-
-
+const MONGGOD_CONNECT_URI =
+  process.env.MONGGOD_CONNECT_URI || env.MONGGOD_CONNECT_URI;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(MONGGOD_CONNECT_URI,console.log('conectoDB'))
+// mongoose.connect(MONGGOD_CONNECT_URI,console.log('conectoDB'));
 
-app.get('/',(req,res)=>{
-    modeluser.find({})
-    .then((users) => res.json(users))
-    .catch((err) => res.json(err));
-  
-});
-
-
-app.get('/signup',(req,res)=>{
-    modeluser.find({})
-    .then((users) => res.json(users))
-    .catch((err) => res.json(err));
-  
-});
-
-app.post('/',(req,res)=>{
-    modeluser.create(req.body)
-    .then((users) => res.json(users))
-    .catch((err) => res.json(err));
-});
-
-app.post('/signup',(req,res)=>{
-    modeluser.create(req.body)
+const conectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      MONGGOD_CONNECT_URI,
+      console.log("conectoDB")
+    );
+    console.log(`MongoDB CONECTO: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+app.get("/", (req, res) => {
+  modeluser
+    .find({})
     .then((users) => res.json(users))
     .catch((err) => res.json(err));
 });
 
-app.listen(PORT,()=>{
+app.get("/signup", (req, res) => {
+  modeluser
+    .find({})
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
+app.post("/", (req, res) => {
+  modeluser
+    .create(req.body)
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
+app.post("/signup", (req, res) => {
+  modeluser
+    .create(req.body)
+    .then((users) => res.json(users))
+    .catch((err) => res.json(err));
+});
+
+
+conectDB().then(() => {
+  app.listen(PORT, () => {
     console.log(`escuchando...${PORT}`);
-})
-
-
-
+  });
+});
